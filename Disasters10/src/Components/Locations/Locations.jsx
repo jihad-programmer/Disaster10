3711bdbc54096418c21a8c4ecdc87a8dd58d12ec
jihad-programmer <mdjaminur2300@gmail.com>
2024-10-20 00:08:27 -0700

    Location's page all Image + First section Done

--- Disasters10/src/Components/Locations/Locations.jsx
+++ Disasters10/src/Components/Locations/Locations.jsx
@@ -7,13 +7,32 @@ function Locations() {
 
     <NavTitle P='Incidents - DR-4699 March 2023 Severe Storms' H='DR-4699 March 2023 Severe Storms' B='+New Location'/>
 
-    <div className='w-[100%]'>
+    <div className='w-[100%] flex'>
 
-      <div className='w-[50%] h-[1582.5px] space-y-10 mx-auto p-5'>
-
+      <div className='w-[50%] h-[1582.5px] space-y-10 m-5 p-5 bg-slate-200'>
+           <div className='w-[428px] h-[123px] space-y-[45px]'>
+               <div className='h-[39px] flex gap-2'>
+                  <div>
+                    <img src="/LO.png" alt="" />
+                  </div>
+                  <div>
+                    <h1 className='text-[14px] text-[#6B7280] font-[400] font-onest'>Location</h1>
+                    <p className='font-[700] text-[20px] font-onest'>Tulare County,  Los Angles, CA 23415</p>
+                  </div>
+               </div>
+               <div className='h-[39px] flex gap-2'>
+                  <div>
+                    <img src="/LO.png" alt="" />
+                  </div>
+                  <div>
+                    <h1 className='text-[14px] text-[#6B7280] font-[400] font-onest'>Approx. Cost:</h1>
+                    <p className='font-[700] text-[20px] font-onest'>$60,607,456.00</p>
+                  </div>
+               </div>
+           </div>
       </div>
       
-      <div className='w-[50%] h-[565px] space-y-5'>
+      <div className='w-[50%] h-[565px] m-5 space-y-5 bg-sky-400'>
 
       </div>
     </div>
