3c7b363a63e40ef3edf97d2c41a11760738cc4a3
jihad-programmer <mdjaminur2300@gmail.com>
2024-10-19 22:16:11 -0700

    Location page's main Two container done with responsive

--- Disasters10/src/Components/Locations/Locations.jsx
+++ Disasters10/src/Components/Locations/Locations.jsx
@@ -6,6 +6,17 @@ function Locations() {
     <>
 
     <NavTitle P='Incidents - DR-4699 March 2023 Severe Storms' H='DR-4699 March 2023 Severe Storms' B='+New Location'/>
+
+    <div className='w-[100%]'>
+
+      <div className='w-[50%] h-[1582.5px] space-y-10 mx-auto p-5'>
+
+      </div>
+      
+      <div className='w-[50%] h-[565px] space-y-5'>
+
+      </div>
+    </div>
         
     </>
   )
